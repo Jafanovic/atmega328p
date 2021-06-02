5459ca57b2e77c0fc25e24aef5f675c66c716c14
Jaroslav Fait <jardak.fait@gmail.com>
2021-06-02 07:02:21 +0200

    repo init

---
+++ test01/Inc/main.h
@@ -0,0 +1,16 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+ /* Includes ------------------------------------------------------------------*/
+
+ /* Private define ------------------------------------------------------------*/
+#define LED4_Pin GPIO_PIN_12
+#define LED4_GPIO_Port GPIOD
+#define LED3_Pin GPIO_PIN_13
+#define LED3_GPIO_Port GPIOD
+#define LED5_Pin GPIO_PIN_14
+#define LED5_GPIO_Port GPIOD
+#define LED6_Pin GPIO_PIN_15
+#define LED6_GPIO_Port GPIOD
+
+#endif /* __MAIN_H */
